c1602b316d04637b4451913130ac7564126ef7dc
Alberto Almagro <alberto.almagro@netcentric.biz>
2016-03-01 09:34:13 +0100

    Create project foundation

---
+++ bundle/src/main/java/biz/netcentric/bulkcontentdownloader/HelloService.java
@@ -0,0 +1,13 @@
+package biz.netcentric.bulkcontentdownloader;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
